c271e711e4c534571309e85741705813c21f65b9
Rishabh Sharma <78745855+Rishabh5752@users.noreply.github.com>
2024-05-09 09:47:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,18 @@ import gradio as gr
 import numpy as np
 from tensorflow.keras.models import load_model
 
-# Load the trained model
+
 model = load_model('skin_model.h5')
 
-# Define a function to make predictions
+
 def predict(image):
-    # Preprocess the image
     image = image / 255.0
     image = np.expand_dims(image, axis=0)
     
-    # Make prediction using the model
+    
     prediction = model.predict(image)
     
-    # Get the predicted class label
+    
     if prediction[0][0] < 0.5:
         label = 'benign'
     else:
@@ -24,7 +23,7 @@ def predict(image):
 
 examples=[["benign.jpg"], ["malignant.jpg"]]
 
-# Define a Gradio interface for user interaction
+
 image_input = gr.inputs.Image(shape=(150, 150))
 label_output = gr.outputs.Label()
 
